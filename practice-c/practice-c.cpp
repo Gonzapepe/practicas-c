52294e9bb6be8dadb6b4e03170c23b563022e130
Gonzaloaddamo <addamogonzalo@gmail.com>
2022-08-09 18:42:12 -0300

    first commit

---
+++ practice-c/practice-c.cpp
@@ -0,0 +1,12 @@
+﻿// practice-c.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "practice-c.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
